e0ca81c8e2f001f6e8182f44abd2f79afe2bb5c9
Joey Korkames <kfix@users.noreply.github.com>
2022-06-18 14:28:18 -0700

    more tuning

--- src/main.cpp
+++ src/main.cpp
@@ -99,13 +99,12 @@ void readFromMicom(int byteCount)
     }
     uint8_t reg = I2C_micom.read();
     uint8_t val = I2C_micom.read();
-    bool frobbled = false;
     bool verbose = false;
     // NOTE these register maps are model specific!
     // TODO: make this a map struct/class so we can work with a diff model
     switch (reg)
     {
-      case REG_OSD_RGB_MODE: // is continously set on interval by Orion TV!
+      case REG_OSD_RGB_MODE: // is continously set on interval by Orion TV to 0xa0 => 0b10100000
         // bit7 // AFC2 H Phase (MSB bit)
         // bit6 // Not Assigned
         // bit5 // Fsc free | Free-running mode of crystal oscillator | 0: OFF | 1: Free-running
@@ -114,39 +113,53 @@ void readFromMicom(int byteCount)
         // bit2 // Force Color 1=>ON 0=>OFF
         // bit0 // Color Killer Sensitivity Threshold Switch | 0: 43dB | 1: 45dB
 
-        bitSet(val, 4);  // ensure "Analog OSD"
-        frobbled = true;
+        bitSet(val, 4); // ensure "Analog OSD"
+        // I put diodes inline with the OSD RGBS to protect the chip so Analog-mode needs to stay on
+        // the diodes' added voltage-drop prevents Digital High from being read at all
+        // so its not even theroretically usable when RGB switch is OFF
         break;
-      case 0x2: // is continously set on interval by Orion TV to 0x80 => '0b10000000'
+/*
+      case 0x2: // is continously set on interval by Orion TV to 0x8 => '0b00001000'
         //  bit7 // Luminance signal Mute ON/OFF switch | 0: Out | 1: Mute
         //  bit6 // AF Direct out/External Audio input signal switch | 0: AF amp out | 1: External
         //  bit5 // Forced Spot Killer under Power on condition | 0: Forced S.Killer | 1: Off
         //  bit4 // Y-signal Chroma Trap ON/OFF switch | 0: Chroma Trap ON | 1: Chroma Trap Off
-        //  bit3 // Video Tone Gain (Hi/Normal) switch | 0: normal | 1: high(sharp)
+        //  bit3 // Video Tone Gain (Hi/Normal) switch | 0: normal | 1: high(sharp) [orion default]
         //  bit2 // ABCL | 0 => OFF (ACL) | 1 => ON
         //  bit1 // Luminance Signal Delay time Fine pitch Adjustment
         //  bit0 // Chroma BPF/Take-Off Function Switch | 0: BPF | 1: Take-Off
         if (rgb_switched) {
-          // if sync line is Comp-video, we'd get "double-exposed" over saturation unless we mute
-          // bitSet(val, 7);
-        } else {
-          //bitClear(val, 7);
+          // if sync input is CVBS(Comp-video), I see "double-exposed" color
+          // (excess luminance)
+          // FIXME: mute that color-video processing!
+          //  i don't think its possible with the jungle settings..
+          //   might just have to install a sync cleaner and switch it on,
+          //   then switch to EXT input which is wired to the cleaner's output
+          //  or maybe i just need to increase my blanking voltage?
+          bitSet(val, 7); // maybe ignore luminance of Y-in?
+          bitSet(val, 4); // maybe don't process colors of Y-in?
+            //picture still works, but still over-luminated
+          bitClear(val, 3); // no diff...
+          bitSet(val, 2); // no diff....
         }
-        //verbose = true;
+        verbose = true;
         break;
-      case 0x06: // is continously set on interval by Orion TV to 0xa0 => '0b10100000'
+*/
+      case 0x06: // is continously set on interval by Orion TV to either
+      // 0xa0 => '0b10100000'
+      // 0xb0 => '0b10110000' (after v-latchings?)
         // bit5:7 // VIF Video Out Gain
         // bit4 // AV Switch Selector |  V-Latch | 0: Composite video input | 1: Y/C input mode
         // bit3 // Black Stretch function ON/OFF switch | 0: ON | 1: OFF
         // bit2 // AV Switch Selector |  V-Latch | 0: TV mode | 1: EXT mode
         // bit0:1 // Luminance Signal Delay time Adjustment
         if (rgb_switched) {
-          //bitSet(val, 4);
-          //bitSet(val, 2);
+          // bitSet(val, 4); // input be CSync, ignore CVBS
+             // no change for RGB+CVBSync, still overluminated
+             // RGBS doesn't seem to need it changed
         } else {
-          bitClear(val, 4); // 1 shows CVBS' luminance as a monochrome picture
+          bitClear(val, 4); // be CVBS, else its luminance is shown as a monochrome picture
         }
-        verbose = true;
         break;
       //case 0x0:
         // bit7 // Inter Carrier/Split Carrier Switch 0: Inter Carrier, 1: Split Carrier
@@ -180,22 +193,39 @@ void readFromMicom(int byteCount)
       //case 0x0b:
         // bit7 // Vertical Forced free-running mode switch | 0: OFF | 1: Forced Free-running
         // bit0:6 // R OUT Amplitude Adjustment by 7bit DAC
-      //case 0x10: // continous 0x14
+      //case 0x10: // continously written - HVCO varies with calibration -'0b00XXX100'
         // bit7 // White Raster Mode Switch | 1=>ON 0=>OFF
         // bit6 // Sync Det Slice Level (50%/40%) | 0: 50% | 1: 40%
-        // bit3-5 // H VCO free-running frequency Adjustment | 0%-100%
+        // bit3-5 // H VCO free-running frequency Adjustment | 0%-100% | (Orion Service Menu #04)
+        // bit0:2 // Not Assigned
       //case 0x11:
         // bit0-5 // V-Size V RAMP Amplitude Adjustment by 6bit DAC.
-      //case 0x12:
+      //case 0x12: // continously 0xb0 => 0b10110000
         // bit4-7 // Intelligent Monitor mode selector
-        // bit2-3 // Luminance Gamma Threshold Control 0:Gamma OFF
-      //case 0x13: //continuous 0x12 => '0b00010010'
-        // bit7 // Horizontal Forced free-running mode switch | 0: OFF |1: Forced Free-running
+        // bit2-3 // Luminance Gamma Threshold Control | 0:Gamma OFF
+        // bit0-2 // TRAP Fine Adj
+      case 0x13: // continuously set 0x12 => '0b00010010' (at boot, Orion makes 1 request to 0x11 => '0b00010001')
+        // bit7 // Horizontal Forced free-running mode switch | 0: OFF | 1: Forced Free-running
         // bit6 // Vertical Sync. Det mode (1 Window/2 Window) | 0: 2 Window/Vsyncdet=9μs | 1: 1Window/Vsyncdet=11μs
-        // bit5 // Horizontal AFC Gain switch | 0: Low | 1: High
-        // bit4 // H-Start
-        // bit3 // Service SW
-        // bit0:2 // V-Shift V RAMP Sart timing Adjustment 2Line/Step
+        // bit5 // Horizontal AFC Gain switch | 0: Low | 1: High << is twitching in svc menu
+        // bit4 // Horizontal output switch | 0: H-deflection Off | 1: H-deflection On
+        // bit3 // Service Switch | 0: V-Out ON, Contrast-Control Normal | 1: V-Out off, Contrast Control Minimum
+                // triggered by Orion Service Menu #01 to create a single-horizontal line raster for yoke calibration
+        // bit0:2 // V-Shift V RAMP Start timing Adjustment 2Line/Step
+
+        // Orion Service menu flips AFC-Gain => ON randomly, shifting the picture horizontally
+        // (OEM bug? or because I've taken over RF input for CVBS/sync input but tuner isn't really tuned into anything)
+        // AFC-Gain doesn't appear to be needed for RGBS input to work...& maybe it adds lag?
+        // but CVBS really seems to need it (picture smears at the top without)
+        // so lets just force it on always to keep the geometry consistent across both modes
+        bitClear(val, 5);
+
+        if (rgb_switched) {
+          // lets keep V-deflection always on too...
+          //  have seen this get flicked randomly during non-svc-menu use (dirty bus line??)
+          bitClear(val, 3);
+        }
+        break;
       //case 0x14: // continuous 0x74 => 0b01110100
         // bit7 // Pin6 FBP slice level switch | 0:Vth=2V(narrow) | 1:Vth=1V(wide)
         // bit6 // Y SW OUT frequency switch | 0: FLAT | 1: LPF(fc=700KHz)
@@ -211,12 +241,6 @@ void readFromMicom(int byteCount)
       // lets do _one_ retry
       delay(50);
       writeToJungle(reg, val, verbose);
-    }
-    // could also put [reg, val] in a circular buffer that loop() flushes on interval,
-    //   instead of proxying from the handler
-
-    if (frobbled) {
-      // Serial.println("OSD RGB analogging bit frobbled!");
     }
 }
 
